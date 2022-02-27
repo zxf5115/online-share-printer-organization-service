91434685b27aad239b95ca33618951fadbb2c2d8
Zhang <1326336909@qq.com>
2022-02-27 21:57:00 +0800

    修复问题

--- app/Http/Controllers/Api/Module/Organization/AssetController.php
+++ app/Http/Controllers/Api/Module/Organization/AssetController.php
@@ -102,6 +102,8 @@ class AssetController extends BaseController
 
       $condition = self::getCurrentWhereData($field);
 
+      $condition['bind_status'] = 1;
+
       $already_printer_total = Printer::getCount($condition);
 
       $response['printer_total']         = $printer_total;
